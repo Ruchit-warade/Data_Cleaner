e0fa91785ee14e5182f9ae2240e5b5fc71bbd10e
Ruchit-warade <warade.ruchit05@gmail.com>
2026-06-29 18:00:32 +0530

    Update Loader

---
+++ src/loader.py
@@ -0,0 +1,4 @@
+import pandas as pd  
+import numpy as np
+
+file = pd.read_csv(input("Please enter file path :- "))
---
+++ src/main.py
@@ -0,0 +1,5 @@
+import pandas as pd   
+import numpy as np
+import loader,cleaner,analyser,reporter,utils
+
+print(loader.file)
